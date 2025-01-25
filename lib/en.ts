f99ff1371f6bb7af7696b805b9442fea38ab620d
mohamadhosein <mhmk1399@gmail.com>
2025-01-25 20:36:22 +0330

    sad

---
+++ lib/en.ts
@@ -0,0 +1,40 @@
+export default {
+    "bannerText": {
+      "text": "Comprehensive solutions for industrial excellence",
+      "imageAlt": "about",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac semper augue. Sed euismod, nunc vel tincidunt ultricies, sapien nunc aliquet nunc, euismod euismod nunc nunc euismod nunc."
+    },
+    "pricing": {
+      "Beginner": {
+        "title": "Beginner",
+        "description": "Etiam ac convallis enim, eget euismod dolor.",
+        "features": {
+          "Aenean quis": "Aenean quis",
+          "Morbi semper": "Morbi semper",
+          "Tristique enim nec": "Tristique enim nec"
+        }
+      },
+      "Pro": {
+        "title": "Pro",
+        "description": "Morbi cursus ut sapien sit amet consectetur.",
+        "features": {
+          "Everything in Free": "Everything in Free",
+          "Phasellus tellus": "Phasellus tellus",
+          "Praesent faucibus": "Praesent faucibus",
+          "Aenean et lectus blandit": "Aenean et lectus blandit"
+        }
+      },
+      "Team": {
+        "title": "Team",
+        "description": "Phasellus ultrices bibendum nibh in vehicula.",
+        "features": {
+          "Everything in Pro": "Everything in Pro",
+          "Fusce sem ligula": "Fusce sem ligula",
+          "Curabitur dictum": "Curabitur dictum",
+          "Duis odio eros": "Duis odio eros",
+          "Vivamus ut lectus ex": "Vivamus ut lectus ex"
+        }
+      },
+      "buttonText": "Get Started"
+    }
+  }  
